f94d7de9ee19a8965c853f482d729b74b50f3c49
erion-meta <metaerion79@gmail.com>
2023-04-05 16:12:40 +0200

    SetUp sign up page, MailChimp

--- 9_API/MailChimp/server.js
+++ 9_API/MailChimp/server.js
@@ -4,6 +4,21 @@ const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signUp.html");
+});
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let Email = req.body.email;
+
+  console.log(firstName, lastName, Email);
+});
+
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
 });
