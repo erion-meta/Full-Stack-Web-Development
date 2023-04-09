89d82be20606112a6961ee88828a0547735373a3
erion-meta <metaerion79@gmail.com>
2023-04-09 15:43:42 +0200

    Posting data to mailchimp servers

--- 9_API/MailChimp/server.js
+++ 9_API/MailChimp/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const client = require("@mailchimp/mailchimp_marketing");
 const request = require("request");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
+
+client.setConfig({
+  apiKey: "964905e0ac6bef34b38508b8ca686286-us8",
+  server: "us8",
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signUp.html");
@@ -14,11 +20,35 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   let firstName = req.body.fName;
   let lastName = req.body.lName;
-  let Email = req.body.email;
+  let email = req.body.email;
+  const listId = "defc59a6df";
 
-  console.log(firstName, lastName, Email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  const run = async () => {
+    const response = await client.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+  };
+
+  console.log(firstName, lastName, email);
+
+  run();
+  res.send("Data posted to Mailchimp servers.");
 });
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
 });
+
+/// API Key
+/// 964905e0ac6bef34b38508b8ca686286-us8
