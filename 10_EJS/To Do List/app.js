5af21b3b8c9ebea6d02e6de438f97483342bc415
erion-meta <metaerion79@gmail.com>
2023-04-12 00:27:08 +0200

    First EJS template

--- 10_EJS/To Do List/app.js
+++ 10_EJS/To Do List/app.js
@@ -9,7 +9,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "saturday";
+      break;
+
+    case 1:
+      day = "monday";
+      break;
+
+    case 2:
+      day = "tuesday";
+      break;
+
+    case 3:
+      day = "wednesday";
+      break;
+
+    case 4:
+      day = "thursday";
+      break;
+
+    case 5:
+      day = "friday";
+      break;
+
+    case 6:
+      day = "saturday";
+      break;
+
+    default:
+      day = "day!";
+      break;
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
