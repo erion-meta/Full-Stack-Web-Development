f16b8371ea577f8467f0ef230119819fb4130cb9
erion-meta <metaerion79@gmail.com>
2023-04-14 23:42:29 +0200

    Adding Work route

--- 10_EJS/To Do List/app.js
+++ 10_EJS/To Do List/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // variables
 let items = ["Sleep", "Eat", "Pray"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -22,13 +23,23 @@ app.get("/", (req, res) => {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
   let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => {
