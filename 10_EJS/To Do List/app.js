3e3cdd28157f95ac5f8e199908f21316f6b3df1a
erion-meta <metaerion79@gmail.com>
2023-04-15 16:18:23 +0200

    Node modules export

--- 10_EJS/To Do List/app.js
+++ 10_EJS/To Do List/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -13,21 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/css", express.static("css"));
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work List") {
     workItems.push(item);
@@ -43,7 +35,7 @@ app.get("/work", (req, res) => {
 });
 
 app.post("/work", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
