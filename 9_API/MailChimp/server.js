e37a07e1ada6b25a8c27e8c6c229426ce7edcc14
erion-meta <metaerion79@gmail.com>
2023-04-16 15:04:54 +0200

    request uninstall

--- 9_API/MailChimp/server.js
+++ 9_API/MailChimp/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const client = require("@mailchimp/mailchimp_marketing");
-const request = require("request");
 
 const app = express();
 
