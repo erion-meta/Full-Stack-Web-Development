057107fe854e182edbe499d9c6d4e48b3d83d571
erion-meta <metaerion79@gmail.com>
2023-04-16 15:54:02 +0200

    Adds header.ejs and footer.ejs

--- 11_Blog Website/app.js
+++ 11_Blog Website/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 
 const app = express();
 app.set("view engine", "ejs");
